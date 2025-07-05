650ad5591520a7047ab695971bf7c901c83ce85a
mateo751 <mateoelopez20000@gmail.com>
2025-07-05 16:47:09 -0500

    laboratorio de ci/cd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import jenkinsLogo from './image/207-2078637_jenkins-ci-clipart.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={jenkinsLogo} className="App-logo" alt="Jenkins logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hola mundo desde jenkis
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
